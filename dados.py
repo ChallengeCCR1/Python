4304d3eddf5ad7d8576fd56b9d0d266151d902ba
devpedrosena1 <devpedrosena1@gmail.com>
2025-04-16 17:44:53 -0300

    Retirando os avisos estáticos do sistema, já que agora, está integrado com a API de informações da linha.

--- dados.py
+++ dados.py
@@ -1,8 +1,2 @@
 usuarios = {}
-avisos = [
-    "Linha 4 Amarela com grande fluxo de passageiros neste momento.", #0
-    "Linha 8 Diamante com atrasos de 10 minutos.", #1
-    "Linha 9 Esmeralda operando normalmente."#2
-]
-
 viagens = []
